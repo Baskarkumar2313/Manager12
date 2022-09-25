9f973cb03ee82732abd5c503b155b982ba136d38
BASKAR.K <110029191+Baskarkumar2313@users.noreply.github.com>
2022-09-25 12:19:55 +0530

    Add files via upload

---
+++ Manager12.java
@@ -0,0 +1,29 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+public class Manager12{
+
+public static void main(String[] args){
+ 
+List<String> list1=new ArrayList<>();
+
+list1.add("java");
+list1.add("raja");
+list1.add("python");
+list1.add("angular");
+list1.add("spring");
+list1.add("spring boot");
+list1.add("Rest API");
+list1.add("1");
+list1.add("2");
+list1.add("3");
+list1.add("4");
+list1.add("5");
+list1.add("6");
+list1.add("7");
+System.out.println(list1);
+}
+}
+
+
